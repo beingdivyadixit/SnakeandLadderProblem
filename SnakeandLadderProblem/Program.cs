e85ed022183124227c76ee1a9bac12390a138568
Divya <beingdivyadixit@gmail.com>
2023-04-11 15:31:08 +0530

    UC-1

--- SnakeandLadderProblem/Program.cs
+++ SnakeandLadderProblem/Program.cs
@@ -7,6 +7,9 @@ namespace SnakeandLadderProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world to Snake and Ladder Problem");
+            Console.WriteLine("Begin Game");
+            int Position = 0;
+            Console.WriteLine("Player is in Position {0}", Position); //Player is on 0th Position
         }
     }
 }
