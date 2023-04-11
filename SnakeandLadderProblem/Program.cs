8e0ee73a09075395a329d19777fc17639d727e9f
Divya <beingdivyadixit@gmail.com>
2023-04-11 15:29:21 +0530

    hello world

---
+++ SnakeandLadderProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeandLadderProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world to Snake and Ladder Problem");
+        }
+    }
+}
